ca3827387ba5643c014251fde4efa46e45bcfe86
Muzammil Shabbir <muzammil.techliance.com>
2024-12-08 19:08:32 +0500

    margin fix

--- src/app/components/TodoItem/TodoItem.tsx
+++ src/app/components/TodoItem/TodoItem.tsx
@@ -25,7 +25,7 @@ const TodoItem: React.FC<Props> = ({ todo }) => {
         onChange={toggleComplete}
         className="w-5 mt-3.5 h-5 text-purple-500 border-gray-300 cursor-pointer rounded-full focus:ring-purple-500"
       />
-      <span className={`w-full border-t border-gray-300 pt-3 ${todo.completed ? 'line-through text-gray-500' : ''}`}>
+      <span className={`w-full ml-1 border-t border-gray-300 pt-3 ${todo.completed ? 'line-through text-gray-500' : ''}`}>
         {todo.text}
       </span>
     </div>
